f51539e06ea063ac42d397f6831f2724e95da5fe
Rafael Camps <krzyboss@gmail.com>
2023-10-12 21:15:24 +0200

    Created project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { IconBrain, IconHeart } from '@tabler/icons-svelte';
+</script>
+
+<main class="flex flex-col justify-center items-center h-screen bg-slate-200">
+    <div class="flex gap-x-3">
+        <IconBrain size={54} class={'text-pink-300'} />
+        <h1 class="text-5xl text-green-500">Memory Game</h1>
+    </div>
+    <h2 class="text-2xl text-gray-700">Cooming soon</h2>
+</main>

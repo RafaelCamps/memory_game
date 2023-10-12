6bf5dbdaac3b045eb552677cba05980c3d4595c9
Rafael Camps <krzyboss@gmail.com>
2023-10-12 21:26:44 +0200

    Added favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,20 @@
     import { IconBrain, IconHeart } from '@tabler/icons-svelte';
 </script>
 
+<svelte:head>
+    <title>Memory game</title>
+</svelte:head>
+
+
 <main class="flex flex-col justify-center items-center h-screen bg-slate-200">
     <div class="flex gap-x-3">
         <IconBrain size={54} class={'text-pink-300'} />
         <h1 class="text-5xl text-green-500">Memory Game</h1>
     </div>
     <h2 class="text-2xl text-gray-700">Cooming soon</h2>
+    <footer>
+        by @Rafactorizando
+    </footer>
 </main>
+
+

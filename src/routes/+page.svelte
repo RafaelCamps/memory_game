3533f626262e461a211448531f196ddc8dd303cd
Rafael Camps <krzyboss@gmail.com>
2023-10-13 19:42:20 +0200

    Created states & started building the game

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
     import { IconBrain, IconHeart } from '@tabler/icons-svelte';
+    import { emojiFood } from '$lib/emoji-food.ts'
+
+    type State = 'start' | 'playing' | 'paused' | 'won' | 'lost'
+
+    let state:State = 'start'
+    let size = 20
+    let grid = createGrid()
+
+    const createGrid = () => {
+        let cards = new Set<string>()
+        let maxSize = size / 2
+    }
+
 </script>
+
 
 <svelte:head>
     <title>Memory game</title>
@@ -12,7 +26,7 @@
         <IconBrain size={54} class={'text-pink-300'} />
         <h1 class="text-5xl text-green-500">Memory Game</h1>
     </div>
-    <h2 class="text-2xl text-gray-700">Cooming soon</h2>
+    <h2 class="text-2xl text-gray-700">Cooming soon {emojiFood[10]}</h2>
     <footer>
         by @Rafactorizando
     </footer>
